d6d2f1a0f9266c8a22afe234b3ab6c15f65a7b5e
tianhong229 <wth@tianhongs-MacBook-Pro.local>
2018-05-28 14:00:47 +0800

    add solution for prune tree

---
+++ src/main/java/com/tianhong/BinaryTreePruning.java
@@ -0,0 +1,87 @@
+package com.tianhong;
+
+public class BinaryTreePruning {
+
+    public static boolean printTree(NodePoint root) {
+        if(root == null) {
+            return false;
+        }
+        printTree(root.getRight());
+        System.out.println("root: "+root.getVal());
+        printTree(root.getLeft());
+
+        return true;
+    }
+
+    public static NodePoint pruneTree(NodePoint root) {
+        if(root == null || (root.getRight() == null && root.getLeft() == null && root.getVal() == 0)) {
+            return null;
+        }
+        NodePoint leftTree = pruneTree(root.getLeft());
+        NodePoint rightTree = pruneTree(root.getRight());
+
+        if(leftTree == null) {
+            root.setLeft(null);
+        }
+
+        if(rightTree == null) {
+            root.setRight(null);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        NodePoint root = new NodePoint(1);
+        NodePoint node1 = new NodePoint(2);
+        NodePoint node2 = new NodePoint(0);
+        NodePoint node3 = new NodePoint(0);
+        NodePoint node4 = new NodePoint(0);
+        NodePoint node5 = new NodePoint(0);
+
+        root.setLeft(node1);
+        root.setRight(node2);
+        node1.setLeft(node3);
+        node1.setRight(null);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+
+        NodePoint newRoot = pruneTree(root);
+        printTree(newRoot);
+
+    }
+
+}
+
+class NodePoint {
+    private int val;
+    private NodePoint left;
+    private NodePoint right;
+
+    public NodePoint(int val) {
+        this.val = val;
+    }
+
+    public void setLeft(NodePoint left) {
+        this.left = left;
+    }
+
+    public void setRight(NodePoint right) {
+        this.right = right;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public NodePoint getRight() {
+        return this.right;
+    }
+
+    public NodePoint getLeft() {
+        return this.left;
+    }
+
+    public int getVal() {
+        return this.val;
+    }
+}
