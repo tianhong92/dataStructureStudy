15b3964642e418f8ed1fdb1f94106e8de5a7c593
tianhong <tianhong229@gmail.com>
2018-07-16 14:02:15 +0800

    quick sort java implement

--- src/main/java/com/tianhong/QuickSort.java
+++ src/main/java/com/tianhong/QuickSort.java
@@ -1,29 +1,42 @@
 package com.tianhong;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort {
 
-    public static int[] quickSort(int[] arr, int startIndex, int endIndex) {
-        if(startIndex >= endIndex) {
-            return new int[]{};
+    public static void quickSort(List<Integer> list) {
+        int length = list.size();
+        if(length == 0) {
+            return;
         }
-        int pivot = arr[startIndex];
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(pivot);
-        int count = 0;
-        for(int i = startIndex; i < endIndex; i++) {
-            if(arr[i + 1] < pivot) {
-                 list.add(0, arr[i+1]); 
-                 count++;
+        if(length == 1) {
+            System.out.println(list.get(0));
+            return;
+        }
+        List<Integer> left = new ArrayList<Integer>();
+        List<Integer> right = new ArrayList<Integer>();
+        int pivot = list.get(0);
+        for(int i = 1; i < length; i++) {
+            if(list.get(i) >=  list.get(0)) {
+                right.add(list.get(i));
             } else {
-                list.add(arr[i+1]);
+                left.add(list.get(i));
             }
+        }
+
+        if(left.size() != 0) {
+            quickSort(left);
+        }
+        System.out.println(pivot);
+        if(right.size() != 0) {
+            quickSort(right);
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(454545);
+        List<Integer> test = Arrays.asList(2, 6, 4, 8, 1);
+        quickSort(test);
     }
 }
---
+++ src/main/java/com/tianhong/QuickSortExchange.java
@@ -0,0 +1,54 @@
+package com.tianhong;
+
+public class QuickSortExchange {
+
+    public static int divide(int[] arr, int start, int end) {
+        while(start < end) {
+            while(start < end && arr[start] <= arr[end]) {
+                start++;
+            }
+            if(arr[start] > arr[end]) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                end--;
+            }
+
+            while(start < end && arr[start] < arr[end]) {
+                end--;
+            }
+            if(arr[end] < arr[start]) {
+                int temp = arr[end];
+                arr[end] = arr[start];
+                arr[start] = temp;
+            }
+        }
+        return start;
+    }
+
+    public static void sort(int[] a, int start, int end) {
+        if(start > end) {
+            return;
+        }
+        int partition = divide(a, start, end);
+        sort(a, start, partition - 1);
+        sort(a, partition + 1, end);
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] a = new int[]{2,7,4,5,10,1,9,3,8,6};
+        int[] b = new int[]{1,2,3,4,5,6,7,8,9,10};
+        int[] c = new int[]{10,9,8,7,6,5,4,3,2,1};
+        int[] d = new int[]{1,10,2,9,3,2,4,7,5,6};
+
+        sort(c, 0, a.length-1);
+
+        System.out.println("排序后的结果：");
+        for(int x : c){
+            System.out.print(x+" ");
+        }
+    }
+
+}
