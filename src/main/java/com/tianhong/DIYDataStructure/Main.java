94ec79e04c21a52fc04726ec2fd15bfed4e48f30
tianhong229 <wth@tianhongs-MBP.lan>
2018-08-07 22:53:40 +0800

    test stack

--- src/main/java/com/tianhong/DIYDataStructure/Main.java
+++ src/main/java/com/tianhong/DIYDataStructure/Main.java
@@ -8,7 +8,10 @@ public class Main {
         ArrayStack<Integer> stack = new ArrayStack<>();
         for(int i = 0; i < 5; i++) {
             stack.push(i);
+            System.out.println(stack);
         }
+
+        stack.pop();
         System.out.println(stack);
     }
 }
