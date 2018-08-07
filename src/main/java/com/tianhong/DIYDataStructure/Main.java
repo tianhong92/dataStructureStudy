955b37584cdd34ed1724c542861d2671cfdc1cbe
tianhong229 <wth@tianhongs-MBP.lan>
2018-08-07 22:52:22 +0800

    test stack

--- src/main/java/com/tianhong/DIYDataStructure/Main.java
+++ src/main/java/com/tianhong/DIYDataStructure/Main.java
@@ -1,23 +1,15 @@
 package com.tianhong.DIYDataStructure;
 
 import com.tianhong.DIYDataStructure.Array.Array;
+import com.tianhong.DIYDataStructure.Stack.ArrayStack;
 
 public class Main {
     public static void main(String[] args) {
-        Array<Integer> arr = new Array<>(10);
+        ArrayStack<Integer> stack = new ArrayStack<>();
         for(int i = 0; i < 5; i++) {
-            arr.addLast(i);
+            stack.push(i);
         }
-        System.out.println(arr);
-        for(int i = 0; i < 10; i++) {
-            arr.addLast(i);
-        }
-        System.out.println(arr);
-        arr.removeLast();
-        for(int i =0; i<10; i++){
-            arr.removeLast();
-        }
-        System.out.println(arr);
+        System.out.println(stack);
     }
 }
 
