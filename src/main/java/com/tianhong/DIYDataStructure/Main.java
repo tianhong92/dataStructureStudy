b11cb5e5df1be5ad3b178e0e5f8fcccd332e4441
tianhong <tianhong229@gmail.com>
2018-08-07 15:50:54 +0800

    add git ignore

--- src/main/java/com/tianhong/DIYDataStructure/Main.java
+++ src/main/java/com/tianhong/DIYDataStructure/Main.java
@@ -3,7 +3,12 @@ package com.tianhong.DIYDataStructure;
 public class Main {
     public static void main(String[] args) {
         Array arr = new Array(10);
-
+        for(int i = 0; i < 5; i++) {
+            arr.addLast(i);
+        }
+        System.out.println(arr);
+        arr.removeLast();
+        System.out.println(arr);
     }
 }
 
