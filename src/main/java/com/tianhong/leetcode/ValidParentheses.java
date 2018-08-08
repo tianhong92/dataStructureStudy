5e265766c2d757c0adff934f4946fc29ee5974b1
tianhong229 <wth@tianhongs-MacBook-Pro.local>
2018-08-08 17:55:06 +0800

    add parentheses check

--- src/main/java/com/tianhong/leetcode/ValidParentheses.java
+++ src/main/java/com/tianhong/leetcode/ValidParentheses.java
@@ -1,9 +1,38 @@
 package com.tianhong.leetcode;
 
+import com.tianhong.DIYDataStructure.Stack.ArrayStack;
+import com.tianhong.DIYDataStructure.Stack.Stack;
+
 //Given a string containing just the characters '(' , ')' , '{' , '}' , '[' and ']' ,
 //determine if the input string is valid.
 public class ValidParentheses {
-    private boolean checkValidity(String input) {
+    public static boolean checkValidity(String input) {
+        char[] chars = input.toCharArray();
+        Stack<Character> stack = new ArrayStack<>();
+
+        for(char x : chars) {
+            if(x == '(' || x == '{' || x == '[') {
+                stack.push(x);
+            }
+            if(x == ')' || x == '}' || x == ']') {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char y = stack.pop();
+                if( x == '(' && y != ')')
+                    return false;
+                else if (x == '['  && y != ']')
+                    return false;
+                else if (x == '{' && y != '}')
+                    return false;
+
+            }
+        }
+
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkValidity("[(dfd)]"));
     }
 }
