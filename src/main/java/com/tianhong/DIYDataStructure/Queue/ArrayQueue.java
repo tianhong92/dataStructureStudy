0aa82e9e736e0abf4affc06504aad3d2710f0803
tianhong <tianhong229@gmail.com>
2018-08-08 20:20:19 +0800

    add arrayqueue

---
+++ src/main/java/com/tianhong/DIYDataStructure/Queue/ArrayQueue.java
@@ -0,0 +1,68 @@
+package com.tianhong.DIYDataStructure.Queue;
+
+import com.tianhong.DIYDataStructure.Array.Array;
+
+public class ArrayQueue<E> implements Queue<E> {
+    private Array<E> array;
+    public ArrayQueue(int capacity) {
+        array = new Array<>(capacity);
+    }
+
+    public ArrayQueue() {
+        array = new Array<>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity() {
+        return array.getCapacity();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue(){
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront(){
+        return array.getFirst();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Queue: ");
+        builder.append("front [");
+        for(int i = 0; i < array.getSize(); i++){
+            builder.append(array.get(i));
+            if(i != array.getSize() - 1){
+                builder.append(", ");
+            }
+        }
+        builder.append("] tail");
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for(int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            if(i%4 == 3) {
+                System.out.println(queue);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/tianhong/DIYDataStructure/Queue/Queue.java
@@ -0,0 +1,9 @@
+package com.tianhong.DIYDataStructure.Queue;
+
+public interface Queue<E> {
+    int getSize();
+    boolean isEmpty();
+    void enqueue(E e);
+    E dequeue();
+    E getFront();
+}
