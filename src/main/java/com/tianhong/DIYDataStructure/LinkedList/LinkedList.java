14c8b998b476d5fbf26af0e7a6211724d1da5b84
tianhong <tianhong229@gmail.com>
2018-08-09 17:01:33 +0800

    wip: linkedlist

--- src/main/java/com/tianhong/DIYDataStructure/LinkedList/LinkedList.java
+++ src/main/java/com/tianhong/DIYDataStructure/LinkedList/LinkedList.java
@@ -23,4 +23,66 @@ public class LinkedList<E> {
         }
     }
 
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    //获取链表中元素个数
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    // 在链表头添加新的元素e
+    public void addFirst(E e){
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+        head = new Node(e, head);
+        size++;
+    }
+
+    // 在链表index（0-based)位置添加新的元素e
+    // 在链表中不是一个常用的操作， 练习用， 一般不用索引
+    public void add(int index, E e){
+        if(index < 0 || index > size){
+            throw new IllegalArgumentException("Illegal index");
+        }
+        if(size == 0) {
+            head = new Node(e);
+            size++;
+            return;
+        }
+        Node node = new Node(e);
+        Node preNode = null;
+        Node nextNode = null;
+        for(int i = 0; i < index; i++) {
+            preNode = head;
+            nextNode = head.next;
+            head = nextNode;
+        }
+        preNode.next = node;
+        node.next = nextNode;
+        size++;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for(int i = 0; i < 5; i++){
+            linkedList.add(i, i);
+        }
+        System.out.println(linkedList);
+    }
+
 }
