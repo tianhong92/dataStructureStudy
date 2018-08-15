559f19dfef733905c198849a74cc252957c49e71
tianhong <tianhong229@gmail.com>
2018-08-15 16:35:58 +0800

    wip: remove list node

--- src/main/java/com/tianhong/leetcode/removeListNode/ListNode.java
+++ src/main/java/com/tianhong/leetcode/removeListNode/ListNode.java
@@ -4,81 +4,72 @@ package com.tianhong.leetcode.removeListNode;
 // input:  1->2->6->3->4->5->6, val = 6
 // output: 1->2->3->4->5
 
+
+import org.w3c.dom.NodeList;
+
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) { val = x; }
 
-
-    public static ListNode removeElements1(ListNode head, int val) {
-        while(head != null && head.val == val) {
-            head = head.next;
-        }
-
-        if(head.next == null || head == null)
-            return head;
-
-        ListNode preHead = head;
-        while(head != null && head.next != null) {
-            ListNode removeNode = head.next;
-            ListNode nextNode = removeNode.next;
-            if(removeNode.val == val) {
-                head.next = nextNode;
-                removeNode.next = null;
-            }
-            else
-                head = removeNode;
-        }
-
-        return preHead;
+    ListNode(int x){
+        val = x;
+    }
+    ListNode(int x, ListNode next){
+        val = x;
+        this.next = next;
     }
 
-    // 创建虚拟头结点
-    public static ListNode removeElements2(ListNode head, int val) {
-        ListNode dummyNode = new ListNode(-1);
-        dummyNode.next = head;
-
-        if(head.next == null || head == null)
-            return head;
-
-        ListNode preHead = dummyNode;
-        while(preHead != null && preHead.next != null) {
-            ListNode removeNode = preHead.next;
-            ListNode nextNode = removeNode.next;
-            if(removeNode.val == val) {
-                preHead.next = nextNode;
-                removeNode.next = null;
-            }
-            else
-                preHead = removeNode;
+    public static ListNode removeNode(ListNode head, int target){
+        if(head == null){
+            return null;
         }
+        while(head != null && head.val == target){
+            head = head.next;
+        }
+        if(head == null){
+            return null;
+        }
+        ListNode prev = head;
+        ListNode cur = head.next;
+        while(cur != null){
+            if(cur.val == target){
+                cur = cur.next;
+                prev.next = cur;
+            } else {
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+        return head;
 
-        return dummyNode.next;
     }
 
     public static void main(String[] args) {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(2);
-        ListNode node4 = new ListNode(1);
+        ListNode node7 = new ListNode(7, null);
+        ListNode node6 = new ListNode(3, node7);
+        ListNode node5 = new ListNode(5, node6);
+        ListNode node4 = new ListNode(3, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(3, node3);
+        ListNode node1 = new ListNode(3, node2);
 
-
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = null;
-
-        ListNode newNode = ListNode.removeElements2(node1, 2);
-        while(newNode.next != null) {
-            System.out.println(newNode.val);
-            newNode = newNode.next;
-            if(newNode.next == null)
-                System.out.println(newNode.val);
+        ListNode head = node1;
+        while (head != null){
+            System.out.print(head.val+" ");
+            head = head.next;
         }
+
+        removeNode(node1, 3);
+        System.out.println();
+
+        ListNode head2 = node1;
+        while (head2 != null){
+            System.out.print(head2.val+" ");
+            head2 = head2.next;
+        }
+
     }
 }
-
-
 
 
 
