247d57d864b283f02de0c7c3db1db8e99bf66c50
tianhong <tianhong229@gmail.com>
2018-08-15 11:01:09 +0800

    compare linkedlist stack and array stack effectiveness

--- src/main/java/com/tianhong/DIYDataStructure/Main.java
+++ src/main/java/com/tianhong/DIYDataStructure/Main.java
@@ -2,17 +2,30 @@ package com.tianhong.DIYDataStructure;
 
 import com.tianhong.DIYDataStructure.Array.Array;
 import com.tianhong.DIYDataStructure.Stack.ArrayStack;
+import com.tianhong.DIYDataStructure.Stack.LinkListStack;
+import com.tianhong.DIYDataStructure.Stack.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for(int i = 0; i < 5; i++) {
+    public static double opTime(int opCount, Stack<Integer> stack){
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i < opCount; i++){
             stack.push(i);
-            System.out.println(stack);
         }
+        for(int j = 0; j < opCount; j++) {
+            stack.pop();
+        }
+        long endTime = System.currentTimeMillis();
+        return (endTime - startTime)/1000.0;
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new ArrayStack<>();
+        Stack<Integer> stack2 = new LinkListStack<>();
+        int opCount = 100000;
+        double opTime1 = opTime(opCount, stack);
+        double opTime2 = opTime(opCount, stack2);
 
-        stack.pop();
-        System.out.println(stack);
+        System.out.println("Array stack: "+opTime1);
+        System.out.println("Linked list stack: "+opTime2);
     }
 }
 
