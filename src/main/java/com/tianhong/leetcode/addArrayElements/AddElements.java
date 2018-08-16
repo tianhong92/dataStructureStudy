543cdd79013b3cd0b8c7e423863a6cd9cc0c10f5
tianhong <tianhong229@gmail.com>
2018-08-16 11:31:28 +0800

    get add elements done

--- src/main/java/com/tianhong/leetcode/addArrayElements/AddElements.java
+++ src/main/java/com/tianhong/leetcode/addArrayElements/AddElements.java
@@ -4,15 +4,24 @@ public class AddElements {
     public static void main(String[] args) {
        int[] array = {1, 3, 5, 7, 9};
        AddElements addElements = new AddElements();
-       int ret = addElements.getSum(array, array.length);
+       int ret = addElements.getSum2(array, 0);
         System.out.println(ret);
     }
 
+    // return array[0, l)
     public static int getSum(int[] array, int l){
         if(l == 1){
             return array[0];
         }
         return getSum(array, l-1) + array[l-1];
+    }
+
+    // return array(l， l]
+    public static int getSum2(int[] array, int l) {
+        if(l == array.length - 1){
+            return array[l];
+        }
+        return getSum2(array, l+1) + array[l];
     }
 
 }
