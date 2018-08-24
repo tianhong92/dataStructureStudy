22b517551c9078fce2934f46693a35c13802b229
tianhong <tianhong229@gmail.com>
2018-08-24 15:47:37 +0800

    add get tree depth method

--- src/main/java/com/tianhong/DIYDataStructure/BinarySearchTree/addElements/BSTAddElements.java
+++ src/main/java/com/tianhong/DIYDataStructure/BinarySearchTree/addElements/BSTAddElements.java
@@ -1,5 +1,8 @@
 package com.tianhong.DIYDataStructure.BinarySearchTree.addElements;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BSTAddElements<E extends Comparable<E>> {
     private class Node {
         public E e;
@@ -69,6 +72,29 @@ public class BSTAddElements<E extends Comparable<E>> {
         }
    }
 
+   public int getTreeDepth(){
+       if(size == 0){
+           return 0;
+       }
+       int depth = 0;
+       Queue<Node> queue = new LinkedList<>();
+       queue.add(root);
+       while(!queue.isEmpty()){
+           int length = queue.size();
+           depth++;
+           for(int i = 0; i < length; i++){
+               Node cur = queue.remove();
+               if(cur.left != null){
+                   queue.add(cur.left);
+               }
+               if(cur.right != null){
+                   queue.add(cur.right);
+               }
+           }
+       }
+       return depth;
+   }
+
     private void print(Node root){
         if(root == null){
             return;
@@ -84,12 +110,24 @@ public class BSTAddElements<E extends Comparable<E>> {
 
     public static void main(String[] args) {
         BSTAddElements<Integer> tree = new BSTAddElements<>();
-        tree.add(9);
         tree.add(5);
         tree.add(2);
-        tree.add(11);
-        tree.add(15);
-        tree.print(tree.root);
+        tree.add(1);
+        tree.add(9);
+        tree.add(3);
+        tree.add(7);
+        tree.add(8);
+
+        //////////////////////
+        //          5       //
+        //       /    \     //
+        //     2       9    //
+        //   /   \   /      //
+        // 1     3 7        //
+        //          \       //
+        //           8      //
+        //////////////////////
+        System.out.println(tree.getTreeDepth());
     }
 
 }
