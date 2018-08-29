614f1816017b6f888e578e0c0b2d7dd003898b73
tianhong <tianhong229@gmail.com>
2018-08-29 14:02:27 +0800

    WIP: linkedlist components

--- src/main/java/com/tianhong/leetcode/linkedListQuestions/LinkedListComponents.java
+++ src/main/java/com/tianhong/leetcode/linkedListQuestions/LinkedListComponents.java
@@ -5,4 +5,25 @@ package com.tianhong.leetcode.linkedListQuestions;
 // Output: 2
 // 0 and 1 are connected, 3 and 4 are connected, so [0, 1] and [3, 4] are the two connected components.
 public class LinkedListComponents {
+    private class ListNode {
+        private int val;
+        private ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+        public ListNode(int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 首先遍历原List得到所有connected对存入
+    public int numComponents(ListNode head, int[] G) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
