44f9b026bcd97f5fc30457bd6ee22952a059cb9a
tianhong229 <wth@tianhongs-MBP.lan>
2018-09-08 16:58:09 +0800

    figure out relative path

--- src/main/java/com/tianhong/DIYDataStructure/Set/FileOperation.java
+++ src/main/java/com/tianhong/DIYDataStructure/Set/FileOperation.java
@@ -66,4 +66,11 @@ public class FileOperation {
                 return i;
         return s.length();
     }
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/pride-and-prejudice.txt");
+        System.out.println(file.exists());
+        System.out.println(System.getProperty("user.dir"));
+    }
+
 }
