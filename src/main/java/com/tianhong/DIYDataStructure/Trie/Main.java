a73b5ca96ed8890c8d0cd731878d893f3fa5b5aa
tianhong <tianhong229@gmail.com>
2018-09-25 16:21:58 +0800

    added trie

--- src/main/java/com/tianhong/DIYDataStructure/Trie/Main.java
+++ src/main/java/com/tianhong/DIYDataStructure/Trie/Main.java
@@ -4,22 +4,23 @@ import com.tianhong.DIYDataStructure.Map.FileOperation;
 import com.tianhong.DIYDataStructure.Set.BSTSet;
 
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Pride and prejudice");
         ArrayList<String> words = new ArrayList<>();
-        if(FileOperation.readFile("src/pride-and-prejudice.txt", words)){
+        if(FileOperation.readFile("src/a-tale-of-two-cities.txt", words)){
             long startTime = System.currentTimeMillis();
-            BSTSet<String> set = new BSTSet<>();
+            TreeSet<String> set = new TreeSet<>();
             for(String word : words)
                 set.add(word);
             for(String word : words)
                 set.contains(word);
             long endTime = System.currentTimeMillis();
             double time = (endTime - startTime)/1000.0;
-            System.out.println("Total different words: "+set.getSize());
-            System.out.println("BSTSet: "+time+" s");
+            System.out.println("Total different words: "+set.size());
+            System.out.println("Set: "+time+" s");
 
             long startTime2 = System.currentTimeMillis();
             Trie trie = new Trie();
