42c52b60c13d0598cf2957a3cc629a9e6b1cc0ad
tianhong <tianhong229@gmail.com>
2018-09-26 16:52:36 +0800

    wip: product of array

--- src/main/java/com/tianhong/leetcode/arrayQuestions/ProductOfArrayExceptSelf.java
+++ src/main/java/com/tianhong/leetcode/arrayQuestions/ProductOfArrayExceptSelf.java
@@ -6,20 +6,21 @@ package com.tianhong.leetcode.arrayQuestions;
 public class ProductOfArrayExceptSelf {
     //维护两个数组， fwd[]和bwd[], 分别为当前元素之前所有元素乘积和之后所有元素乘积
     public int[] productExceptSelf(int[] nums) {
-        int[] ret = new int[nums.length];
         int n = nums.length;
-        int[] fwd = new int[n+1];
-        int[] bwd = new int[n+1];
+        int[] fwd = new int[n];
+        int[] bwd = new int[n];
+        int[] ret = new int[n];
         fwd[0] = 1;
-        bwd[n] = 1;
-        for(int i = 1; i <= n; i++){
-            fwd[i] = fwd[i-1] * nums[i-1];
+        for(int i = 1; i < n; i++){
+            fwd[i] = fwd[i-1]*nums[i-1];
         }
-        for(int j = n - 1; j >= 0; j--){
-            bwd[j] = bwd[j+1] * nums[j];
+        bwd[n-1] = 1;
+        for(int j = n -2; j >= 0; j--){
+            bwd[j] = bwd[j+1]*nums[j+1];
         }
+
         for(int k = 0; k < n; k++){
-            ret[k] = fwd[k] * bwd[k+1];
+            ret[k] = fwd[k] * bwd[k];
         }
         return ret;
     }
