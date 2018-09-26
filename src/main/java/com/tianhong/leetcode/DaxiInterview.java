4bc16ea853d3916a79234674b3b260beefb8b644
tianhong <tianhong229@gmail.com>
2018-09-26 17:16:14 +0800

    fuck daxi

--- src/main/java/com/tianhong/leetcode/DaxiInterview.java
+++ src/main/java/com/tianhong/leetcode/DaxiInterview.java
@@ -2,21 +2,22 @@ package com.tianhong.leetcode;
 
 public class DaxiInterview {
     public static void sort(int[] arr, int start, int end) {
-        if(start == end) {
-            System.out.println("Max integer:" + arr[start]);
+        if(end - start == 1) {
+            System.out.println(arr[start] > arr[end] ? arr[start] : arr[end]);
+            return;
         }
-        int midIndex = (int) Math.floor((start + end)/2);
-        if(arr[midIndex] > arr[arr.length -1]) {
-            sort(arr, start, midIndex);
-        }
-        if(arr[midIndex] <= arr[arr.length -1]) {
+        int midIndex = (end + start)/2;
+        if(arr[midIndex] > arr[arr.length - 1]) {
             sort(arr, midIndex, end);
+        }
+        if(arr[midIndex] <= arr[arr.length - 1]) {
+            sort(arr, start, midIndex);
         }
     }
 
 
     public static void main(String[] args) {
-        int[] arr = new int[]{7, 8, 9, 10, 1, 2, 3, 5, 6};
+        int[] arr = new int[]{7, 8, 9, 10, 45, 78, 1, 2, 3, 5, 6};
         sort(arr, 0, arr.length -1);
     }
 }
