ddb9251126f21e3d9f074e1eade4a1af10792d66
tianhong229@gmail.com <tianhong229@gmail.com>
2018-12-04 14:45:43 +0800

    hha

---
+++ src/main/java/com/tianhong/Jianzhi/Re8NextNode.java
@@ -0,0 +1,29 @@
+//package com.tianhong.Jianzhi;
+//
+///**
+// * @Author: TianhongWang
+// * @Date: 2018/12/3
+// * @Description
+// */
+//
+//// 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
+//// 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针
+//public class Re8NextNode {
+//    private class TreeLinkNode {
+//        int val;
+//        TreeLinkNode left = null;
+//        TreeLinkNode right = null;
+//        TreeLinkNode next = null;
+//
+//        TreeLinkNode(int val) {
+//            this.val = val;
+//        }
+//    }
+//
+//    public static TreeLinkNode findNext(TreeLinkNode node){
+//        if(node.right == null){
+//            return node.next;
+//        }
+//    }
+//
+//}
