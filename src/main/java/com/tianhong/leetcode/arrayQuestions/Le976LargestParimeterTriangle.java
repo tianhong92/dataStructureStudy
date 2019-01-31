ed58c03a1ead933f9005cfc525c1dfd45d5c3b3a
tianhong229@gmail.com <tianhong229@gmail.com>
2019-01-31 16:40:40 +0800

    wip: largest parimeter triangle

---
+++ src/main/java/com/tianhong/leetcode/arrayQuestions/Le976LargestParimeterTriangle.java
@@ -0,0 +1,18 @@
+package com.tianhong.leetcode.arrayQuestions;
+
+/**
+ * @author TianhongWang
+ * @since 2019/1/30
+ */
+// Given an array A of positive lengths, return the largest perimeter of a triangle with non-zero area,
+// formed from 3 of these lengths.
+// Input: [2,1,2]
+// Output: 5
+
+// Input: [3,2,3,4]
+// Output: 10
+public class Le976LargestParimeterTriangle {
+    public int largestPerimeter(int[] A) {
+
+    }
+}
