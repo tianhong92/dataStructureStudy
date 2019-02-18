cf6d5e98c7cdf3c83d97781e9edb0ecc2df567e2
tianhong229 <wth@tianhongs-MacBook-Pro.local>
2019-02-18 10:58:52 +0800

    rename class

--- src/main/java/com/tianhong/leetcode/arrayQuestions/Le283MoveZeroes.java
+++ src/main/java/com/tianhong/leetcode/arrayQuestions/Le283MoveZeroes.java
@@ -3,7 +3,7 @@ package com.tianhong.leetcode.arrayQuestions;
 // relative order of the non-zero elements.
 // Input: [0,1,0,3,12]
 // Output: [1,3,12,0,0]
-public class MoveZeroes {
+public class Le283MoveZeroes {
     // move 0 to the end of the array, and move forward the elements after the 0
     public static void moveZeroes(int[] nums) {
         int i = 0;
