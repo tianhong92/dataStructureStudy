6b3821d2615cd4d77ca502b9746966e893462d73
tianhong <tianhong229@gmail.com>
2019-02-22 15:15:23 +0800

    added anagram

--- src/main/java/com/tianhong/leetcode/arrayQuestions/Le438FindAnagramsInString.java
+++ src/main/java/com/tianhong/leetcode/arrayQuestions/Le438FindAnagramsInString.java
@@ -6,11 +6,55 @@ package com.tianhong.leetcode.arrayQuestions;
 // [0, 6]
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Le438FindAnagramsInString {
-//    public List<Integer> findAnagrams(String s, String p) {
-//
-//    }
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        if(s.length() == 0 || p.length() == 0){
+            return list;
+        }
+
+        int[] freq = new int[256];
+        for(char c : p.toCharArray()){
+            freq[(int)c]++;
+        }
+
+        int l = 0;
+        int r = -1;
+        while(l < s.length()){
+            if(r+p.length() < s.length()){
+               int[] tempFreq = freq.clone();
+               if(isAnagramString(s.substring(l, r+p.length()+1), tempFreq)){
+                   list.add(l);
+               }
+            }
+            l++;
+            r++;
+        }
+        return list;
+    }
+
+    public boolean isAnagramString(String a, int[] freq){
+        for(char c : a.toCharArray()){
+            freq[(int)c]--;
+            if(freq[(int)c] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
+        Le438FindAnagramsInString test = new Le438FindAnagramsInString();
+        List<Integer> ret = test.findAnagrams(s, p);
+        for(int i : ret){
+            System.out.println(i+" ");
+        }
+
+    }
 
 }
