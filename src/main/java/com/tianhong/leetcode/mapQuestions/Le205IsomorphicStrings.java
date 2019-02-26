e5069db96aad1d06dc77af5f21a6f778b68f8908
tianhong <tianhong229@gmail.com>
2019-02-26 14:16:21 +0800

    map question

--- src/main/java/com/tianhong/leetcode/mapQuestions/Le205IsomorphicStrings.java
+++ src/main/java/com/tianhong/leetcode/mapQuestions/Le205IsomorphicStrings.java
@@ -22,14 +22,14 @@ public class Le205IsomorphicStrings {
         Map<Character, Character> map = new HashMap<>();
 
         for(int i = 0; i < c1.length; i++){
-            if(!map.containsKey(c1[i]) && !map.containsValue(c2[i])){
-                map.put(c1[i], c2[i]);
-            } else if(map.containsKey(c1[i])){
-                if(map.get(c1[i]) != c2[i]){
+            if(!map.containsKey(c1[i])){
+                if(map.containsValue(c2[i]))
                     return false;
-                }
+                else
+                    map.put(c1[i], c2[i]);
             } else {
-                return false;
+                if(map.get(c1[i]) != c2[i])
+                    return false;
             }
         }
         return true;
