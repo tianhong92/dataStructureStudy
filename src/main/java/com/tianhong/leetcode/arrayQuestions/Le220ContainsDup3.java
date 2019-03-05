3a97f99ec3baf333b93aab8344aa9cad0abdc7cb
tianhong <tianhong229@gmail.com>
2019-03-05 10:48:21 +0800

    two dups

---
+++ src/main/java/com/tianhong/leetcode/arrayQuestions/Le220ContainsDup3.java
@@ -0,0 +1,18 @@
+package com.tianhong.leetcode.arrayQuestions;
+// Given an array of integers, find out whether there are
+// two distinct indices i and j in the array such that the absolute difference between nums[i] and
+// nums[j] is at most t and the absolute difference between i and j is at most k.
+
+// Input: nums = [1,2,3,1], k = 3, t = 0
+// Output: true
+
+// Input: nums = [1,0,1,1], k = 1, t = 2
+// Output: true
+
+// Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+// Output: false
+public class Le220ContainsDup3 {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+    }
+}
