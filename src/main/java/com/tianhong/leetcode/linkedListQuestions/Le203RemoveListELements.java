1e486ebce92256d58fd29b980a36c6cc1fd548aa
tianhong <tianhong229@gmail.com>
2019-03-07 10:52:29 +0800

    keep on

--- src/main/java/com/tianhong/leetcode/linkedListQuestions/Le203RemoveListELements.java
+++ src/main/java/com/tianhong/leetcode/linkedListQuestions/Le203RemoveListELements.java
@@ -4,36 +4,53 @@ package com.tianhong.leetcode.linkedListQuestions;
 // Input:  1->2->6->3->4->5->6, val = 6
 // Output: 1->2->3->4->5
 
-public class Le203RemoveListELements {
-
-    private class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x){
-            val = x;
-        }
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
     }
+}
+
+public class Le203RemoveListELements {
 
     public ListNode removeElements(ListNode head, int val) {
         if(head == null)
             return head;
-        ListNode reHead = new ListNode(-1);
-        reHead.next = head;
-        ListNode cur = head;
-        while(cur.val == val && cur != null){
-            cur = cur.next;
-            reHead.next = cur;
-        }
-        ListNode pre = cur;
-        while(cur != null){
-            if(cur.val == val){
-                cur = cur.next;
-                pre .next = cur;
+        ListNode newHead = new ListNode(-1);
+        newHead.next = head;
+        while(newHead.next != null){
+            if(newHead.next.val == val){
+                newHead.next = newHead.next.next;
             } else {
-                cur = cur.next;
-                pre = cur;
+                break;
             }
         }
-        return reHead;
+        ListNode pre = newHead;
+        while(pre.next != null){
+            if(pre.next.val == val){
+                pre.next = pre.next.next;
+            } else {
+                pre = pre.next;
+            }
+        }
+        return newHead.next;
+    }
+
+    public static void main(String[] args) {
+        Le203RemoveListELements test = new Le203RemoveListELements();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(6);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = null;
+        ListNode ret = test.removeElements(node1, 6);
+        System.out.println(ret);
+
     }
 }
