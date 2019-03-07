a141bd4de7ce8ed9caa8e676672ffd5ad6cfbb39
tianhong <tianhong229@gmail.com>
2019-03-07 16:03:48 +0800

    WIP: remove dups form list

--- src/main/java/com/tianhong/leetcode/linkedListQuestions/Le82RemoveDupsFromSortedList.java
+++ src/main/java/com/tianhong/leetcode/linkedListQuestions/Le82RemoveDupsFromSortedList.java
@@ -18,29 +18,51 @@ public class Le82RemoveDupsFromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null)
             return head;
-        ListNode cur = head;
-        while(cur.next != null){
-            if(cur.val == cur.next.val){
-                cur = cur.next;
-            } else {
-                break;
-            }
+        if(head.next.next == null){
+            if(head.val == head.next.val)
+                return null;
+            else
+                return head;
         }
-        ListNode reHead = cur.next;
-        ListNode pre = reHead;
-        cur = cur.next.next;
-        if(cur == null)
-            return reHead;
-        while(cur.next != null){
-            if(cur.val == cur.next.val){
-                cur = cur.next;
-            } else {
-                cur = cur.next;
-                pre.next = cur;
-                pre = cur;
-            }
-        }
-        return reHead;
 
+        ListNode reHead = new ListNode(-1);
+        reHead.next = head;
+        ListNode pre = head;
+        ListNode cur = pre.next;
+
+        while(cur.next != null){
+            if(cur.val != pre.val && cur.val != cur.next.val){
+                reHead.next = cur;
+                pre = cur;
+                cur = cur.next;
+                break;
+            } else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+
+        while(cur.next != null){
+            if()
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Le82RemoveDupsFromSortedList test = new Le82RemoveDupsFromSortedList();
+
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = null;
     }
 }
