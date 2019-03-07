ff6ab7662dfcdaf881bb98a1ddb9a3052077a5df
tianhong <tianhong229@gmail.com>
2019-03-07 11:19:59 +0800

    ordered list remove dups

--- src/main/java/com/tianhong/leetcode/linkedListQuestions/Le82RemoveDupsFromSortedList.java
+++ src/main/java/com/tianhong/leetcode/linkedListQuestions/Le82RemoveDupsFromSortedList.java
@@ -7,8 +7,40 @@ package com.tianhong.leetcode.linkedListQuestions;
 
 // Input: 1->1->1->2->3
 // Output: 2->3
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+    }
+}
 public class Le82RemoveDupsFromSortedList {
-//    public ListNode deleteDuplicates(ListNode head) {
-//
-//    }
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode cur = head;
+        while(cur.next != null){
+            if(cur.val == cur.next.val){
+                cur = cur.next;
+            } else {
+                break;
+            }
+        }
+        ListNode reHead = cur.next;
+        ListNode pre = reHead;
+        cur = cur.next.next;
+        if(cur == null)
+            return reHead;
+        while(cur.next != null){
+            if(cur.val == cur.next.val){
+                cur = cur.next;
+            } else {
+                cur = cur.next;
+                pre.next = cur;
+                pre = cur;
+            }
+        }
+        return reHead;
+
+    }
 }
