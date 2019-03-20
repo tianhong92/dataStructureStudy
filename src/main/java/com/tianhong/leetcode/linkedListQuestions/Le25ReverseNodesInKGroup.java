18e38e21ab7c3a64b02edc86ce9367265938aeb5
tianhong <tianhong229@gmail.com>
2019-03-20 16:35:10 +0800

    wip

--- src/main/java/com/tianhong/leetcode/linkedListQuestions/Le25ReverseNodesInKGroup.java
+++ src/main/java/com/tianhong/leetcode/linkedListQuestions/Le25ReverseNodesInKGroup.java
@@ -12,23 +12,36 @@ class ListNode4 {
 
 public class Le25ReverseNodesInKGroup {
 
-    public ListNode4 reverseKGroup(ListNode4 head, int k) {
-        if(head == null || head.next == null)
-            return head;
-        ListNode4 dummy = new ListNode4(-1);
-        ListNode4 pre = dummy;
-        ListNode4 cur = head;
-
-        for(int i = 0; i < k - 1; i++){
-            ListNode4 mid = cur.next;
-
-        }
-    }
+//    public ListNode4 reverseKGroup(ListNode4 head, int k) {
+//        if(head == null || head.next == null)
+//            return head;
+//        ListNode4 dummy = new ListNode4(-1);
+//        ListNode4 pre = dummy;
+//        ListNode4 cur = head;
+//
+//        for(int i = 0; i < k - 1; i++){
+//            ListNode4 mid = cur.next;
+//
+//        }
+//    }
 
     // 反转（begin， end）区间（不包含）内的节点， 并返回
     // 0 -> 2 -> 3 -> 4 -> 5 -> 6   (0, 5)
     // 0 -> 4 -> 3 -> 2 -> 5 -> 6   @return 2
     public ListNode4 reverse(ListNode4 begin, ListNode4 end){
+        ListNode4 pre = begin.next;
+        ListNode4 cur = pre.next;
+        ListNode4 first = begin.next;
 
+        while(cur != end){
+            ListNode4 next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        begin.next = pre;
+        first.next = cur;
+        return first;
     }
 }
