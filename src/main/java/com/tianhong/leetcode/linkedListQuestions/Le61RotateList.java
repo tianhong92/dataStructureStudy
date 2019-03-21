3f697dfb3194ff0c2cea6034582d7f4faafbf37b
tianhong <tianhong229@gmail.com>
2019-03-21 15:13:35 +0800

    wip rotate list

---
+++ src/main/java/com/tianhong/leetcode/linkedListQuestions/Le61RotateList.java
@@ -0,0 +1,22 @@
+package com.tianhong.leetcode.linkedListQuestions;
+
+// Given a linked list, rotate the list to the right by k places, where k is non-negative.
+// Input: 1->2->3->4->5->NULL, k = 2
+// Output: 4->5->1->2->3->NULL
+// Explanation:
+// rotate 1 steps to the right: 5->1->2->3->4->NULL
+// rotate 2 steps to the right: 4->5->1->2->3->NULL
+
+class ListNode7 {
+    int val;
+    ListNode7 next;
+    ListNode7(int val){
+        this.val = val;
+    }
+}
+
+public class Le61RotateList {
+    public ListNode7 rotateRight(ListNode7 head, int k) {
+
+    }
+}
