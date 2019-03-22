c2a93261ced6c6f4700e642801f788f7d8bd7de5
tianhong <tianhong229@gmail.com>
2019-03-22 19:18:50 +0800

    done mim height of BTree

--- src/main/java/com/tianhong/leetcode/treeQuestions/Le111MinimumDepthOfBTree.java
+++ src/main/java/com/tianhong/leetcode/treeQuestions/Le111MinimumDepthOfBTree.java
@@ -1,5 +1,7 @@
 package com.tianhong.leetcode.treeQuestions;
 
+// The minimum depth is the number of
+// nodes along the shortest path from the root node down to the nearest "leaf node".
 // Given binary tree [3,9,20,null,null,15,7],
 //
 //    3
@@ -15,6 +17,9 @@ public class Le111MinimumDepthOfBTree {
     public int minDepth(TreeNode root) {
         if(root == null)
             return 0;
-        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        if(root.left == null || root.right == null)
+            return minDepth(root.left) + minDepth(root.right) + 1;
+        else
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
 }
