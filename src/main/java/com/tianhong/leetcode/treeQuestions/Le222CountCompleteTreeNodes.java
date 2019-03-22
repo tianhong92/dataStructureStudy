ee7861e921fab9b7d2705df19aa072f41cf8a2d8
tianhong <tianhong229@gmail.com>
2019-03-22 18:56:06 +0800

    WIP tree problems

---
+++ src/main/java/com/tianhong/leetcode/treeQuestions/Le222CountCompleteTreeNodes.java
@@ -0,0 +1,15 @@
+package com.tianhong.leetcode.treeQuestions;
+
+//      Input:
+//
+//           1
+//          / \
+//         2   3
+//        / \  /
+//       4  5 6
+//
+//      Output: 6
+
+public class Le222CountCompleteTreeNodes {
+
+}
