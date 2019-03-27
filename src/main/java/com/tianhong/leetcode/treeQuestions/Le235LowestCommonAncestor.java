78a9f0df577f7baafe6a729c87e47168640fa6d3
tianhong <tianhong229@gmail.com>
2019-03-27 15:09:47 +0800

    added lowest common ancestor

---
+++ src/main/java/com/tianhong/leetcode/treeQuestions/Le235LowestCommonAncestor.java
@@ -0,0 +1,38 @@
+package com.tianhong.leetcode.treeQuestions;
+
+import sun.reflect.generics.tree.Tree;
+
+public class Le235LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || (root.left == null && root.right == null))
+            return null;
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(6);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(8);
+        TreeNode n4 = new TreeNode(0);
+        TreeNode n5 = new TreeNode(4);
+        TreeNode n6 = new TreeNode(7);
+        TreeNode n7 = new TreeNode(9);
+
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        Le235LowestCommonAncestor test = new Le235LowestCommonAncestor();
+        TreeNode ret = test.lowestCommonAncestor(n1, n2, n5);
+        System.out.println(ret.val);
+
+    }
+}
