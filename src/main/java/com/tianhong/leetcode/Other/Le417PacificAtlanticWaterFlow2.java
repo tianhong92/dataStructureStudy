0cd036b5baecef773c91bb8db127510f9ed77554
tianhong229@gmail.com <tianhong229@gmail.com>
2019-04-22 16:54:42 +0800

    Le 417 pacific atlantic flow

---
+++ src/main/java/com/tianhong/leetcode/Other/Le417PacificAtlanticWaterFlow2.java
@@ -0,0 +1,73 @@
+package com.tianhong.leetcode.Other;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author TianhongWang
+ * @since 2019/4/22
+ */
+public class Le417PacificAtlanticWaterFlow2 {
+
+    private List<int[]> list = new ArrayList<>();
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        if(matrix == null || matrix.length < 1)
+            return list;
+        int x = matrix.length;
+        int y = matrix[0].length;
+
+        boolean[][] reachAtlantic = new boolean[x][y];
+        boolean[][] reachPacific = new boolean[x][y];
+        // left atlantic
+        for(int i = 0; i < x; i++) {
+            reach(matrix, reachAtlantic, i, 0, matrix[i][0]);
+        }
+        // up atlantic
+        for(int j = 1; j < y; j++) {
+            reach(matrix, reachAtlantic, 0, j, matrix[0][j]);
+        }
+        // right pacific
+        for(int k = 0; k < x; k++) {
+            reach(matrix, reachPacific, k, y - 1, matrix[k][y-1]);
+        }
+        // bot pacific
+        for(int l = 0; l < y - 1; l++){
+            reach(matrix, reachPacific, x - 1, l, matrix[x-1][l]);
+        }
+        for(int t = 0; t < x; t++){
+            for(int g = 0; g < y; g++){
+                if(reachAtlantic[t][g] && reachPacific[t][g]){
+                    list.add(new int[]{t, g});
+                }
+            }
+        }
+        return list;
+    }
+
+    public void reach(int[][] matrix, boolean[][] reachable, int x, int y, int pre) {
+        if(!inBoard(matrix, x, y)){
+            return;
+        } else {
+            if(reachable[x][y]) {
+                return;
+            } else if (matrix[x][y] >= pre) {
+                reachable[x][y] = true;
+            } else {
+                return;
+            }
+        }
+        reachable[x][y] = true;
+        // up
+        reach(matrix, reachable,x - 1, y, matrix[x][y]);
+        // right
+        reach(matrix, reachable, x, y + 1, matrix[x][y]);
+        // down
+        reach(matrix, reachable,x + 1, y, matrix[x][y]);
+        // left
+        reach(matrix, reachable, x, y - 1, matrix[x][y]);
+    }
+
+    public boolean inBoard(int[][] matrix, int x, int y){
+        return x >= 0 && y >= 0 && x < matrix.length && y <matrix[0].length ? true : false;
+    }
+}
