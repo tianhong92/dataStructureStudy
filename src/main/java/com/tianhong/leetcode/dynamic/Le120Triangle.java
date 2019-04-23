43514693f02c5afd2342fae3ceab6118545ebc05
tianhong229@gmail.com <tianhong229@gmail.com>
2019-04-23 19:33:56 +0800

    start triangle question

---
+++ src/main/java/com/tianhong/leetcode/dynamic/Le120Triangle.java
@@ -0,0 +1,13 @@
+package com.tianhong.leetcode.dynamic;
+
+import java.util.List;
+
+/**
+ * @author TianhongWang
+ * @since 2019/4/23
+ */
+public class Le120Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+    }
+}
