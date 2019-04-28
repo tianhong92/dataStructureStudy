f9d0aae69a95ff3b5e175b25556239c2890581bf
wth <wth@wthdeMacBook-Pro.local>
2019-04-28 14:58:42 +0800

    added le120 triangle

--- src/main/java/com/tianhong/leetcode/dynamic/Le120Triangle.java
+++ src/main/java/com/tianhong/leetcode/dynamic/Le120Triangle.java
@@ -10,7 +10,15 @@ import java.util.List;
  */
 public class Le120Triangle {
     public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.size() < 1)
+            return 0;
+        int[] minimum = new int[triangle.size() + 1];
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                minimum[j] = triangle.get(i).get(j) + Math.min(minimum[j], minimum[j + 1]);
+            }
+        }
 
+        return minimum[0];
     }
-
 }
