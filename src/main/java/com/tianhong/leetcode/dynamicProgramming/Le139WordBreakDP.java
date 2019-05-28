de875885329ba796eb1a19ac99e266d46b969b3b
wth <wth@wthdeMacBook-Pro.local>
2019-05-28 21:00:39 +0800

    added dp solution

---
+++ src/main/java/com/tianhong/leetcode/dynamicProgramming/Le139WordBreakDP.java
@@ -0,0 +1,19 @@
+package com.tianhong.leetcode.dynamicProgramming;
+
+import java.util.List;
+
+public class Le139WordBreakDP {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 0; j <= i; j++) {
+                if(dp[j] && wordDict.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
