2f6e9cc92f081b7f4d39bbc2df1047b35c33edd6
wth <wth@wthdeMacBook-Pro.local>
2019-06-03 19:33:59 +0800

    wip le 739

---
+++ src/main/java/com/tianhong/leetcode/arrayQuestions/Le739DailyTemperatures.java
@@ -0,0 +1,35 @@
+package com.tianhong.leetcode.arrayQuestions;
+
+public class Le739DailyTemperatures {
+    public int[] dailyTemperatures(int[] T) {
+        int[] ret = new int[T.length];
+        for(int i = 0; i < T.length; i++) {
+            for(int j = i; j < T.length; j++) {
+                if(T[j] > T[i]) {
+                    ret[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+
+    public int[] dailyTemperatures2(int[] T) {
+        int[] ret = new int[T.length];
+        int i = 0;
+        int j = 1;
+        while(i < T.length - 1) {
+            if(j <= T.length - 1 && T[j] > T[i]) {
+                ret[i] = j - i;
+                i++;
+                j = i + 1;
+            } else if (j < T.length - 1) {
+                j++;
+            } else {
+                i++;
+                j = i + 1;
+            }
+        }
+        return ret;
+    }
+}
