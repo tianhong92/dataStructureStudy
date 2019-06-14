abf43220aa24e1bbc0de352bc95ce1cdabe4b2cf
wth <wth@wthdeMacBook-Pro.local>
2019-06-14 09:44:10 +0800

    added le 787

---
+++ src/main/java/com/tianhong/leetcode/Other/Le787CheapestFlightsWithKStops.java
@@ -0,0 +1,29 @@
+package com.tianhong.leetcode.Other;
+
+public class Le787CheapestFlightsWithKStops {
+    private int total;
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        total = Integer.MAX_VALUE;
+        backTrack(flights, src, dst, K, 0, 0);
+        return total == Integer.MAX_VALUE ? -1 : total;
+    }
+
+    public void backTrack(int[][] flights, int src, int dst, int K, int level, int price) {
+        if(level > K + 1){
+            return;
+        }
+        if(src == dst && price < total) {
+            total = price;
+            return;
+        }
+        for(int i = 0; i < flights.length; i++) {
+            int[] flight = flights[i];
+            int s = flight[0];
+            if(s == src) {
+                int d = flight[1];
+                int p = flight[2];
+                backTrack(flights, d, dst, K, level + 1, price + p);
+            }
+        }
+    }
+}
